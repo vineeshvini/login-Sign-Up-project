91527c51c25160e75caf53b56962efd082b6deee
vineesh <vineeshvinijg@gmailcom>
2024-03-20 13:41:08 +0530

    Add untracked files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
+
+
+function App() {
+  return (
+    <div>
+    <LoginSignUp/>
+    </div>
+  )
+}
+
+export default App;
